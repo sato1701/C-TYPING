85cf3e707509f4b5ba65c722883b42e53f0e4e13
sato1701 <a3city17@gmail.com>
2020-09-09 20:06:37 +0900

    タイピングに使う単語をランダムに表示させる機能を追加

--- typing_game.c
+++ typing_game.c
@@ -6,33 +6,42 @@
 #include <math.h>
 #include <windows.h>
 
+#define cont_count_MAX 20 //一時的に使用　後に、プレイヤーが設定
+
 struct Words{
-	char word[15];
+	char word[20];
 	_Bool WasUsed;
-} contents[50];
+} contents[100];
 
 int data_read();
 void calc_show(double, int, int);
 
 int main(){
 	const int FILE_NUM = data_read();
-	int c_index = 0;
-	int w_index = 0;
+	int cont_count = 0;
+	int cont_index = 0;
+	int word_index = 0;
 	int all_count = 0;
 	int success_count = 0;
 	char tmp;
-	char show_word[15] = "\0";
+	char show_word[20] = "";
 	clock_t start, end;
 
+	srand((unsigned)time(NULL));
 	start = clock();
 
-	while(c_index < FILE_NUM){
-		while(contents[c_index].word[w_index] != '\0'){
+	while(cont_count < cont_count_MAX){
+		do{
+			cont_index = rand() % FILE_NUM;
+		}while(contents[cont_index].WasUsed);
+		contents[cont_index].WasUsed = TRUE;
+
+		while(contents[cont_index].word[word_index] != '\0'){
 
 			system("cls");
 			printf("typing miss ... %2d\n\n", all_count - success_count);
-			puts(contents[c_index].word);
-			strncpy(show_word, contents[c_index].word, w_index);
+			puts(contents[cont_index].word);
+			strncpy(show_word, contents[cont_index].word, word_index);
 			puts(show_word);
 
 			tmp = getch();
@@ -41,17 +50,18 @@ int main(){
 				printf("強制終了！");
 				exit(0);
 			}
-			if(tmp == contents[c_index].word[w_index]){
-				w_index++;
+			if(tmp == contents[cont_index].word[word_index]){
+				word_index++;
 				success_count++;
 			}
 			all_count++;
 		}
-		c_index++;
-		w_index = 0;
-		for(int k = 0; k < 15; k++)
+		cont_count++;
+		word_index = 0;
+		for(int k = 0; k < 20; k++)
 			show_word[k] = '\0';
 	}
+
 	end = clock();
 
 	calc_show((double)(end-start) / CLOCKS_PER_SEC, success_count, all_count);
@@ -60,7 +70,7 @@ int main(){
 
 int data_read(){
 	FILE *datafile;
-	int file_num = 0;
+	int file_index = 0;
 	int word_end;
 
 	datafile = fopen("WordsFile.txt", "r");
@@ -69,16 +79,16 @@ int data_read(){
 		exit(1);
 	}
 
-	fscanf(datafile, "%d\n", &file_num);
-	for(int i=0; i < file_num; i++){
-		fgets(contents[i].word, 20, datafile);
-		word_end = strlen(contents[i].word)-1;
-		contents[i].word[word_end] = '\0';
+	//fscanf(datafile, "%d\n", &file_index);
+	while((fgets(contents[file_index].word, 20, datafile)) != NULL){
+		word_end = strlen(contents[file_index].word)-1;
+		contents[file_index].word[word_end] = '\0';
+		file_index++;
 	}
 
 	fclose(datafile);
 
-	return file_num;
+	return file_index;
 }
 
 void calc_show(double time_diff, int success, int all){
